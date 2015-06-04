292dc86eed3cf78fc78179f854374f7e7c2abd11
codedecde <barunpatra95@gmail.com>
2015-06-04 22:28:23 +0530

    Moved the files to the K Mean Folder

--- Code/Source/debugKMean.m
+++
@@ -1,9 +0,0 @@
-function mincost = debugKMean(X)
-	%Testing for K = 1 to 10
-	mincost = zeros(10,2);
-	for i = 1:10;
-		mincost(i,1) = i;
-		[dummy1,dummy2,mincost(i,2)] = runKMean(X,i);
-	end
-plot(mincost(:,1),mincost(:,2),'r');
-end
