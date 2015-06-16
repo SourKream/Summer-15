c67179aa55b7ac5c7d9ba9a5425c2b43fff2bc39
Shantanu Kumar <kumarshantanu95@gmail.com>
2015-06-16 16:56:02 +0530

    We are getting there

--- Code/Source/cost.m
+++ Code/Source/cost.m
@@ -1,7 +1,7 @@
-function cost = cost(data, peak)
+function cost = cost(data, peak, epsilon)
 
-	cost = sigmoid(max(abs(findGradient(data)))-2.5);
+	cost = sigmoid(max(abs(findGradient(data)))-1.5);
 	[xx, x] = max(data);
-	cost = cost * sin(pi * (exp(-(((x-peak)^2)/(0.5))) - 0.5));
+	cost = cost * sin(pi * (exp(-((((x-peak)/epsilon)^2)*(log(2)))) - 0.5));
 
 end
