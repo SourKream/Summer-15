b25d9b2148ac10b939328e33096ecc635cc9e41d
Shantanu Kumar <kumarshantanu95@gmail.com>
2015-06-17 12:25:51 +0530

    Plot Cost

--- Code/Source/cost.m
+++ Code/Source/cost.m
@@ -1,6 +1,6 @@
 function cost = cost(data, peak, epsilon)
 
-	cost = sigmoid(max(abs(findGradient(data)))-1.5);
+	cost = sigmoid(max(abs(findGradient(data)))-2.5);
 	[xx, x] = max(data);
 	cost = cost * sin(pi * (exp(-((((x-peak)/epsilon)^2)*(log(2)))) - 0.5));
 
