262b933ea5ff7ac234886811a3cdc1a0e1fb5c51
Shantanu Kumar <kumarshantanu95@gmail.com>
2015-06-18 17:05:50 +0530

    Cost Function nearly there

--- Code/Source/cost.m
+++ Code/Source/cost.m
@@ -1,7 +1,15 @@
 function cost = cost(data, peak, epsilon)
 
-	cost = sigmoid(max(abs(findGradient(data)))-2.5);
-	[xx, x] = max(data);
-	cost = cost * sin(pi * (exp(-((((x-peak)/epsilon)^2)*(log(2)))) - 0.5));
+%	cost = sigmoid(max(abs(findGradient(data)))-2.5);
+	data = (data - mean(data))./std(data);
+	[maxData, posMaxData] = max(data);
+
+	x = find(data >= max(data) - std(data));
+	y = data(x);
+	cost = sum(y./(1 + (x-posMaxData).^2)) / length(y);
+	cost = 1 - exp(-cost);
+
+	cost = cost *  sin(pi * (exp(-((((posMaxData-peak)/epsilon)^2)*(log(2)))) - 0.5));
 
 end
+
--- Code/Source/plotCostForData.m
+++ Code/Source/plotCostForData.m
@@ -3,6 +3,15 @@ function plotCost = plotCostForData (data, peak, epsilon, option='b')
 	X = data(:,2:end);
 	index = data(:,1);
 	plotCost = [];
+
+	[a,b] = max(X,'',2);
+	if ~exist('epsilon','var') || isempty(epsilon)
+		epsilon = sqrt(mean((b - mode(b)).^2));
+	end
+	if ~exist('peak','var') || isempty(peak)
+		peak = mode(b);
+	end
+	
 	for i=1:size(X,1)
 		plotCost = [plotCost; cost(X(i,:), peak, epsilon)];
 	end
