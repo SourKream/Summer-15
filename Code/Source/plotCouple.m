a5f4f3a3086601f720e2da2f3a61d6d0b4f18834
codedecde <barunpatra95@gmail.com>
2015-06-21 01:51:44 +0530

    Query Genome added

---
+++ Code/Source/plotCouple.m
@@ -0,0 +1,8 @@
+function plotCouple (x, A, B)
+
+	plot(x, A,'o','color','b');
+	hold on;
+	plot(x, B,'o','color','r');
+	hold off;
+
+end
---
+++ Code/Source/queryGenome.m
@@ -0,0 +1,13 @@
+function resultVector = queryGenome(indexVector)
+	numQuery = length(indexVector);
+	resultVector = cell(numQuery,1);
+	fid = fopen('GenomeList.txt');
+	n = 372;
+	genomelist = cell(n,1);
+	for i = 1:n
+		line = fgets(fid);
+		[name,ext] = strtok(line,'.');
+		genomelist{i} = strtrim(name);
+	end
+	resultVector = genomelist(indexVector,:);
+end
