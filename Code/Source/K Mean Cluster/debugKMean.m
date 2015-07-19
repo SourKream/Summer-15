50fe377c1294cfbf45942f85ed805c1a0932a366
codedecde <barunpatra95@gmail.com>
2015-07-19 16:20:36 +0530

    debug K mean changed

--- Code/Source/K Mean Cluster/debugKMean.m
+++ Code/Source/K Mean Cluster/debugKMean.m
@@ -4,6 +4,9 @@ function mincost = debugKMean(X)
 	for i = 1:10;
 		mincost(i,1) = i;
 		[dummy1,dummy2,mincost(i,2)] = runKMean(X,i);
+		fprintf('Cluster number: %2i Done.....\r',i);
+		fflush(stdout);
 	end
+	fprintf('\n');
 plot(mincost(:,1),mincost(:,2),'r');
 end
