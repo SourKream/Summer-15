477e2dfc7d662425f0561e1ebdfab8e7478b8374
Shantanu Kumar <kumarshantanu95@gmail.com>
2015-07-20 21:26:08 +0530

    NewNewNewCost

--- Code/Source/plotCostForData.m
+++ Code/Source/plotCostForData.m
@@ -12,12 +12,6 @@ function plotCost = plotCostForData (data, drawMode ,peak, epsilon, option='b')
 	plotCost = [];
 
 	[a,b] = max(data,'',2);
-	if ~exist('epsilon','var') || isempty(epsilon)
-		epsilon = sqrt(mean((b - mode(b)).^2));
-		if(epsilon==0)
-			epsilon=1;
-		end
-	end
 	if ~exist('peak','var') || isempty(peak)
 
 		%%%%% Added Later	
@@ -25,13 +19,25 @@ function plotCost = plotCostForData (data, drawMode ,peak, epsilon, option='b')
 		for i=1:size(b,1)
 			bb(b(i)) = bb(b(i)) + 1;
 		end
-		[maxb, peak] = max(bb(1:end-4) + bb(2:end-3) + bb(3:end-2) + bb(4:end-1) + bb(5:end));
-		peak = peak + 2;
+		[maxb, peak] = max(bb(1:end-2) + bb(2:end-1) + bb(3:end));
+		[maxb, ppeak] = max(bb(peak : (peak + 2)));
+		peak = peak + ppeak - 1; 
 		%%%%%%%%%%%%%%
 		
 		%peak = mode(b);
 	end
 	
+	if ~exist('epsilon','var') || isempty(epsilon)
+		epsilon = sqrt(mean((b - peak).^2));
+		if(epsilon==0)
+			epsilon=1;
+		end
+		if (epsilon < 2.5)		% Added later
+			epsilon = 2.5;
+		end
+		epsilon;
+	end
+
 	for i=1:size(data,1)
 		plotCost = [plotCost; cost(data(i,:), peak, epsilon)];
 	end
