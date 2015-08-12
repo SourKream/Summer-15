e48dc5c0e4ae738fbaab3c70ea28db96792216d6
Shantanu Kumar <kumarshantanu95@gmail.com>
2015-08-12 13:47:06 +0530

    Input handler and data

--- Code/Source/Optics_python/InputHandler.py
+++ Code/Source/Optics_python/InputHandler.py
@@ -1,5 +1,12 @@
 import Optics as Op
 import numpy as np
+import scipy.io as sio
+
+tempData = sio.loadmat('DataToPython.mat');
+
+ind = tempData['ind'];
+dist = tempData['dist'];
+
 x = np.ones((1500,951));
 y = Op.optics()
 y.setMatrix(x)
